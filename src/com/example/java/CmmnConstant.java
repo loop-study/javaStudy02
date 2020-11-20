03a4b6eb5c9fef5af5d0c96c6a37e3a6d03d21cc
apdkeb <apdkeb@naver.com>
2020-11-20 20:23:13 +0900

    2주차 과제하면서 쓴 코드내용. 그냥 흔적만 남긴다. https://loopstudy.tistory.com/5 블로그를 보자

---
+++ src/com/example/java/CmmnConstant.java
@@ -0,0 +1,17 @@
+package com.example.java;
+
+/**
+ *  클래스 변수만 담긴 클래스.
+ */
+public class CmmnConstant {
+
+    // 지역번호
+    public static final String SEOUL_LOCAL_NUMBER = "02";
+    public static final String INCHEON_LOCAL_NUMBER = "032";
+
+    // 우리집 IP 주소
+    public static final String HOME_IP = "192.0.0.1:8080";
+
+    // API 주소
+    public static final String API_CALL_URL = "https://www.naver.com";
+}
--- src/com/example/java/JavaStudy02.java
+++ src/com/example/java/JavaStudy02.java
@@ -19,6 +19,26 @@ public class JavaStudy02 {
          8) 타입 추론, var
          */
 
+        // 기본형(원시형)의 종류는 정수, 실수, 논리, 문자로 나눠진다
+        // 정수
+        byte b = 1;     // 초기값 0
+        System.out.println(Byte.MAX_VALUE); // 127
+        System.out.println(Byte.MIN_VALUE); // -128
+
+        short s = 1;    // 초기값 0
+        int i = 1;      // 초기값 0
+        long l = 0L;    // 초기값 0L
+
+        // 실수
+        float f = 0f;   // 초기값 0f
+        double d = 0d;  // 초기값 0d
+
+        // 논리
+        boolean is = false; // 초기값 false
+
+        // 문자
+        char c = 'A'; // 초기값 '\u0000'
+
 
         RefTest ref = new RefTest();
         RefTest ref2 = ref;
@@ -39,7 +59,7 @@ public class JavaStudy02 {
         int age;        // int 타입의 age 변수 선언
         double money;   // double 타입의 money 변수 선언
 
-        int a, b, c;    // 같은 타입의 여러개 동시 선언
+        int x, y, z;    // 같은 타입의 여러개 동시 선언
 
         // 초기화 방법도 간단하다 대입 연산자 = 를 사용해서 값을 명시하면 끝. 변수에 초기값을 주는 걸 변수의 초기화라고 뜻한다.
         age = 10;
@@ -61,11 +81,129 @@ public class JavaStudy02 {
         fnScope();
 
         System.out.println(scopeAge);
+
+        // 클래스 변수 접근
+        String url = CmmnConstant.API_CALL_URL;
+        String callNum = CmmnConstant.SEOUL_LOCAL_NUMBER;
+
+        System.out.println(url);
+        System.out.println(callNum);
+
+        // 타입 변환
+        fnCast();
+
+        // 강제 변환
+        fnExpCast();
     }
 
     public static void fnScope () {
         int scopeAge = 20;
         System.out.println(scopeAge);
+    }
+
+    // 자동 변환 예
+    public static void fnCast() {
+        byte byteVal = 10;
+        int intVal = byteVal;   // byte -> int
+        System.out.println(intVal);
+
+        char charVal = '가';
+        intVal = charVal;       // char -> int
+        System.out.println(intVal);
+
+        intVal = 100;
+        long longVal = intVal;  // int -> long
+        System.out.println(intVal);
+
+        intVal = 200;
+        double douVal = intVal; // int -> double
+        System.out.println(intVal);
+    }
+
+    public static void fnExpCast() {
+        byte byteVal = 10;
+        short shortVal = 20;
+        int intVal = 100;
+        long longVal = 1000000L;
+
+        byteVal = (byte) shortVal;
+        shortVal = (short) intVal;
+        intVal = (int) longVal;
+
+        System.out.println("byte : " + byteVal);    // 20
+        System.out.println("short : " + shortVal);  // 100
+        System.out.println("int : " + intVal);      // 1000000
+
+        // 넘치는 값으로 강제 변환하면?
+        shortVal = 32767;
+        intVal = 2100000000;
+        longVal = 2200000000L;
+
+        byteVal = (byte) shortVal;
+        shortVal = (short) intVal;
+        intVal = (int) longVal;
+
+        System.out.println("byte : " + byteVal);    // -1
+        System.out.println("short : " + shortVal);  // 29952
+        System.out.println("int : " + intVal);      // -2094967296
+
+        fnArray();
+    }
+
+    public static void fnArray() {
+        // 수학 평균 점수를 구해보자
+        int sum = 0;
+
+        int score01 = 80;   // 1번 학생
+        int score02 = 80;   // 2번 학생
+        int score03 = 80;   // 3번 학생
+        int score04 = 80;
+        int score05 = 80;
+        // ....
+        int score26 = 80;
+        int score27 = 80;
+        int score28 = 80;
+        int score29 = 80;   // 29번 학생
+        int score30 = 80;   // 30번 학생
+
+        sum += score01;
+        sum += score02;
+        sum += score03;
+        // ...
+        sum += score29;
+        sum += score30;
+
+        int avg = sum / 30;
+
+        // int[] scoreArr = {80, 80, 70, 75, 90, ... , 60, 100};
+        int[] arr;
+        int arr02[];
+        arr = new int[]{100, 90, 80, 70, 60};
+        System.out.println();
+
+        // 2차 배열
+        int[][] doubleArr = new int[3][30];
+
+        // 3차 배열
+        int[][][] triArr = new int[3][5][10];
+
+        int[][][] scoreArr = {
+                {
+                        {100, 90, 80, 70, 80}
+                        , {90, 80, 70, 60, 10}
+                        , {90, 80, 70, 60, 10}
+                }, {
+                {100, 90, 80, 70, 80}
+                , {90, 80, 70, 60, 10}
+                , {90, 80, 70, 60, 10}
+                }, {
+                        {100, 90, 80, 70, 80}
+                        , {90, 80, 70, 60, 10}
+                        , {90, 80, 70, 60, 10}
+                }
+        };
+
+        System.out.println(scoreArr[0][0][0]);
     }
 }
 
@@ -92,24 +230,5 @@ class RefTest {
 
     public String getStr2 () {
         return this.str2;
-    }
-}
-
-class week02 {
-    static int globalAge = 30;
-
-    public static void main(String[] args) {
-        int globalAge = 20;
-        System.out.println(globalAge);          // 20 출력
-
-        // 지역변수가 아닌 전역변수를 언급하려면?
-        System.out.println(week02.globalAge);   // 30 출력
-    }
-
-    public static void fnScope () {
-        int scopeAge = 20;                  // 지역 변수, fnScope 내에서만 사용가능하다.
-
-        System.out.println(scopeAge);
-        System.out.println(globalAge);      // 전역 변수, week02 내에서는 어디든지 접근이 가능
     }
 }
